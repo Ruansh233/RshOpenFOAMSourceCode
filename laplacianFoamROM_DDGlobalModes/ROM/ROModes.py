9d51df3c0097500e3d774cc91e217341cb14c6c4
Shenhui,Ruan <shenhui.ruan@kit.edu>
2022-08-30 17:01:05 +0200

    modification of equ coeff matrix

--- laplacianFoamROM_DDGlobalModes/ROM/ROModes.py
+++ laplacianFoamROM_DDGlobalModes/ROM/ROModes.py
@@ -1,4 +1,4 @@
-from ast import For
+from lib2to3.pgen2.token import RPAR
 from scipy.integrate import solve_ivp
 import numpy as np
 # import matplotlib.pyplot as plt
@@ -8,25 +8,33 @@ filePath = "../svdtest/SVD"
 
 for domainI in range(0, 4):
 
-    ROMdiffucoefficient = filePath + "/subDROMCoeffMatrixdomain" + str(domainI)
-    coeff = filePath + "/subDtemporalCoeffdomain" + str(domainI)
+    ROMdiffucoefficient = filePath + "/subDdiffuCoeffMatrixdomain" + str(domainI)
+    ROMtranscoefficient = filePath + "/subDTransCoeffMatrixdomain" + str(domainI)
+    coeff = filePath + "/temporalCoeffMatrix"
     coeff_calculate = filePath + "/coeff_calculate" + str(domainI)
 
 
     diffuTermCoeffMatrix = np.loadtxt(ROMdiffucoefficient)
-    coeffMatrix = np.loadtxt(coeff)
-    initialA = coeffMatrix[0, :np.shape(diffuTermCoeffMatrix)[1]]
+    transTermCoeffMatrix = np.loadtxt(ROMtranscoefficient)
 
-    print(diffuTermCoeffMatrix)
-    print(initialA)
+    print(np.linalg.det(transTermCoeffMatrix))
 
-    time = np.linspace(0, 50, 501)
+    # ROMCoeffMatrix = np.linalg.inv(transTermCoeffMatrix) * diffuTermCoeffMatrix
+    # coeffMatrix = np.loadtxt(coeff)
+    # initialA = coeffMatrix[0, :np.shape(diffuTermCoeffMatrix)[1]]
+
+    # print(ROMCoeffMatrix)
+
+    # print(initialA)
+    # print("")
+
+    # time = np.linspace(0, 20, 201)
 
 
-    def odefun(t, a):
-        da = 2.5e-3 * diffuTermCoeffMatrix.dot(a)
-        return da
+    # def odefun(t, a):
+    #     da = 2.5e-3 * ROMCoeffMatrix.dot(a)
+    #     return da
 
-    sol = solve_ivp(odefun, [0, 50], initialA, dense_output=True)
+    # sol = solve_ivp(odefun, [0, 20], initialA, dense_output=True)
 
-    np.savetxt(f"{coeff_calculate}", sol.sol(time).T, fmt='%.6e', delimiter=',')
+    # np.savetxt(f"{coeff_calculate}", sol.sol(time).T, fmt='%.6e', delimiter=',')
