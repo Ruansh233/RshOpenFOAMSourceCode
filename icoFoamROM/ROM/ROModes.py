2a1a91fce11c10d7a4ba5e8066f508af8b7ea672
Shenhui,Ruan <shenhui.ruan@outlook.com>
2022-09-02 08:05:40 +0200

    vector transport solver

---
+++ icoFoamROM/ROM/ROModes.py
@@ -0,0 +1,29 @@
+from scipy.integrate import solve_ivp
+import numpy as np
+# import matplotlib.pyplot as plt
+
+filePath = "../svdtest/SVD"
+diffucoefficient = filePath + "/diffuTermCoeffMatrix"
+coeff = filePath + "/coeffMatrix"
+coeff_calculate = filePath + "/coeff_calculate"
+
+diffuTermCoeffMatrix = np.loadtxt(diffucoefficient)
+coeffMatrix = np.loadtxt(coeff)
+initialA = coeffMatrix[0, :np.shape(diffuTermCoeffMatrix)[1]]
+
+print(diffuTermCoeffMatrix)
+print(initialA)
+
+time = np.linspace(0, 50, 501)
+
+
+def odefun(t, a):
+    da = 2.5e-3 * diffuTermCoeffMatrix.dot(a)
+    return da
+
+sol = solve_ivp(odefun, [0, 50], initialA, dense_output=True)
+
+# print(sol.y)
+# print(sol.sol(time))
+
+np.savetxt(f"{coeff_calculate}", sol.sol(time).T, fmt='%.6e', delimiter=',')
