d031a4464ba11a384dcdb0e660674cb5a00e187a
Shenhui,Ruan <shenhui.ruan@outlook.com>
2023-10-27 02:36:25 +0200

    modify it to reconstruct scalar and vector field

--- DGROMresultPlot/DGROMresultPlot.C
+++ DGROMresultPlot/DGROMresultPlot.C
@@ -35,144 +35,180 @@ int main(int argc, char *argv[])
     // add timeSelector to generate all ROM results
     timeSelector::addOptions();
 
+    argList::addOption
+    (
+        "modesFileDir",
+        "word",
+        "Specify the folder of modes file"
+    );
+
+    // Rsh, add dict select function
+    argList::addOption // string variable
+    (
+        "dict",
+        "name",
+        "alternative modePlotDict"
+    );
+    
+    // Initialise OF case
     #include "setRootCase.H"
 
-	// These two create the time system (instance called runTime) and fvMesh (instance called mesh).
+    // These two create the time system (instance called runTime) and fvMesh (instance called mesh).
     #include "createTime.H"
     #include "createMesh.H"
 
-    if (mesh.cellZones().size() == 0)
-    {
-        FatalErrorIn("cellZoneFieldValueROM")
-            << "There is no cellZone in this mesh"
-            << exit(FatalError);
-    }
+    Info<< "Start\n" << endl;
 
-    fileName dataPath (mesh.time().path()/"SVD");
-
-    label elementNum (mesh.cellZones().size());
-
-    // read the matrix
-    // read cell modes matrix
-    fileName dataFile1 (dataPath/"calSnapshotsM");
-    fileName dataFile2 (dataPath/"errorM");
-
-    RectangularMatrix<scalar> calSnapshotsM(mesh.C().size(), elementNum);
-    RectangularMatrix<scalar> errorM(mesh.C().size(), elementNum);
-
-    if(isFile(dataFile1))
-    {                
-        IFstream dataStream(dataFile1);
-        word dataLine;
-        label row(0);
-
-        while(dataStream.getLine(dataLine) && dataLine != word::null)
-        {
-            IStringStream dataString (dataLine);
-            token singleData;  // token stores the data read from IFstream 
-
-            for(label elementI = 0; elementI < elementNum; ++elementI)
-            {
-                dataString.read(singleData);    
-                calSnapshotsM(row, elementI) = singleData.scalarToken();
-            }   
-            ++row;
-        }                       
-    }  
-    else
-    {
-        Info << "file: " << dataFile1 << " is not exist!" << endl;
-        // break;
-    }
-
-    if(isFile(dataFile2))
-    {                
-        IFstream dataStream(dataFile2);
-        word dataLine;
-        label row(0);
-
-        while(dataStream.getLine(dataLine) && dataLine != word::null)
-        {
-            IStringStream dataString (dataLine);
-            token singleData;  // token stores the data read from IFstream 
-
-            for(label elementI = 0; elementI < elementNum; ++elementI)
-            {
-                dataString.read(singleData);    
-                errorM(row, elementI) = singleData.scalarToken();
-            }   
-            ++row;
-        }                       
-    }  
-    else
-    {
-        Info << "file: " << dataFile2 << " is not exist!" << endl;
-        // break;
-    }
-
-    // ---------------------------------------------------------------------------------//
-    // assign cell value to each cellZone
-    // create field value
-    volScalarField T
-    (
-        IOobject
-        (
-            "T",
-            runTime.timeName(),
-            mesh,
-            IOobject::MUST_READ,
-            IOobject::AUTO_WRITE
-        ),
-        mesh
-    );
-    
-    // create mode field by copying T
-    volScalarField calT
-    (
-        IOobject
-        (
-            "calT",
-            mesh.time().timeName(),
-            mesh,
-            IOobject::NO_READ,
-            IOobject::AUTO_WRITE
-        ),
-        T
-    );
-
-    // create mode field by copying T
-    volScalarField error
-    (
-        IOobject
-        (
-            "error",
-            mesh.time().timeName(),
-            mesh,
-            IOobject::NO_READ,
-            IOobject::AUTO_WRITE
-        ),
-        T
-    );
-
-
-    // assign cell value
-    forAll(mesh.cellZones(), ZoneI)
-    {       
-        forAll(mesh.cellZones()[ZoneI], cellI)
-        {
-             
-            calT[mesh.cellZones()[ZoneI][cellI]] = calSnapshotsM(cellI, ZoneI);
-            error[mesh.cellZones()[ZoneI][cellI]] = errorM(cellI, ZoneI);
-        }
-    }
-
-    
     // write the field value to specific time folder
     instantList timeDirs = timeSelector::select0(runTime, args);
     runTime.setTime(timeDirs.last(), 0); 
 
-    calT.write();
-    error.write();
+    fileName dataPath (args.getOrDefault<word>("modesFileDir", runTime.globalPath()/"SVD"));
+
+    // Rsh, the default dict name or input selector
+    //  - must plus the dict select option in the beginning of the code
+    const word dictName("DGReconDict");
+    #include "setSystemMeshDictionaryIO.H"
+    Info<< "Reading " << dictIO.instance()/dictIO.name() << nl << endl;
+    IOdictionary DGReconDict(dictIO);
+
+    // read data from dictionary
+    // List<label> modesNumber (customDict.lookup("modesNumber"));
+    label elementNum (DGReconDict.getLabel("elementNum"));
+    List<word> scalarFileName (DGReconDict.lookup("scalarFileName"));
+    List<word> vectorFileName (DGReconDict.lookup("vectorFileName"));
+    // blockType is different cases, case name is insert after fieldName and before modesNumber
+    // List<label> blockType (customDict.lookup("blockType"));
+
+    RectangularMatrix<scalar> scalarFieldMat(mesh.cellZones()[0].size(), elementNum);
+    RectangularMatrix<scalar> vectorFieldMat(3*mesh.cellZones()[0].size(), elementNum);
+
+    // write scalar field
+    forAll(scalarFileName, nameNo)
+    {
+        fileName dataFile (dataPath/scalarFileName[nameNo]);
+        if(isFile(dataFile))
+        {                
+            IFstream dataStream(dataFile);
+            word dataLine;
+            label row(0);
+
+            while(dataStream.getLine(dataLine) && dataLine != word::null)
+            {
+                IStringStream dataString (dataLine);
+                token singleData;  // token stores the data read from IFstream 
+
+                for(label elementI = 0; elementI < elementNum; ++elementI)
+                {
+                    dataString.read(singleData);    
+                    scalarFieldMat(row, elementI) = singleData.scalarToken();
+                }   
+                ++row;
+            }                       
+        }  
+        else
+        {
+            Info << "file: " << dataFile << " is not exist!" << endl;
+            // break;
+        }
+
+        // create mode field by copying T
+        volScalarField sField
+        (
+            IOobject
+            (
+                scalarFileName[nameNo],
+                mesh.time().timeName(),
+                mesh,
+                IOobject::NO_READ,
+                IOobject::AUTO_WRITE
+            ),
+            mesh,
+            dimless
+        );
+
+        // assign cell value
+        forAll(mesh.cellZones(), ZoneI)
+        {       
+            forAll(mesh.cellZones()[ZoneI], cellI)
+            {       
+                sField[mesh.cellZones()[ZoneI][cellI]] = scalarFieldMat(cellI, ZoneI);
+            }
+        }
+
+        // assign boundary value
+        forAll(sField.boundaryField(), patchI)
+        {
+            sField.boundaryFieldRef().set(patchI, 
+                fvPatchField<scalar>::New("zeroGradient", mesh.boundary()[patchI], sField));
+        }
+
+        sField.write(); 
+    }
+
+    // write vector modes field
+    forAll(vectorFileName, nameNo)
+    {
+        fileName dataFile (dataPath/vectorFileName[nameNo]);
+        if(isFile(dataFile))
+        {                
+            IFstream dataStream(dataFile);
+            word dataLine;
+            label row(0);
+
+            while(dataStream.getLine(dataLine) && dataLine != word::null)
+            {
+                IStringStream dataString (dataLine);
+                token singleData;  // token stores the data read from IFstream 
+
+                for(label elementI = 0; elementI < elementNum; ++elementI)
+                {
+                    dataString.read(singleData);    
+                    vectorFieldMat(row, elementI) = singleData.scalarToken();
+                }   
+                ++row;
+            }                       
+        }  
+        else
+        {
+            Info << "file: " << dataFile << " is not exist!" << endl;
+            // break;
+        }
+
+        volVectorField vField
+        (
+            IOobject
+            (
+                vectorFileName[nameNo],
+                mesh.time().timeName(),
+                mesh,
+                IOobject::NO_READ,
+                IOobject::AUTO_WRITE
+            ),
+            mesh,
+            dimless
+        );
+
+        // assign cell value
+        forAll(mesh.cellZones(), ZoneI)
+        {       
+            forAll(mesh.cellZones()[ZoneI], cellI)
+            {
+                vField[mesh.cellZones()[ZoneI][cellI]] = Vector<scalar>(vectorFieldMat(cellI,                     ZoneI),
+                                                                        vectorFieldMat(cellI + mesh.cellZones()[ZoneI].size(),   ZoneI),
+                                                                        vectorFieldMat(cellI + 2*mesh.cellZones()[ZoneI].size(), ZoneI));
+            }
+        }
+
+        forAll(vField.boundaryField(), patchI)
+        {
+            vField.boundaryFieldRef().set(patchI, 
+                fvPatchField<vector>::New("zeroGradient", mesh.boundary()[patchI], vField));
+        }
+
+        vField.write();      
+    }        
+
 
     Info << "\nEnd\n" << endl;
 
